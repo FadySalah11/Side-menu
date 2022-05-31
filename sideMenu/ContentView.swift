9a1fd579ea4ae66658a6fbac5af1713409451151
fady <fady>
2022-05-31 02:01:28 +0200

    Initial Commit

---
+++ sideMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sideMenu
+//
+//  Created by Folio on 31/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sideMenu/sideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sideMenuApp.swift
+//  sideMenu
+//
+//  Created by Folio on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct sideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
